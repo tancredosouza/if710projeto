954abd5d4c6b226c8a0bbd1b82084b6a1384512c
Tantan <tjssj@cin.ufpe.br>
2019-10-26 13:51:39 -0300

    style(MainActivity, ExampleInstrumentedTest): fix code formatting

--- Appersonal/app/src/androidTest/java/com/example/appersonaltrainer/ExampleInstrumentedTest.kt
+++ Appersonal/app/src/androidTest/java/com/example/appersonaltrainer/ExampleInstrumentedTest.kt
@@ -3,7 +3,9 @@ package com.example.appersonaltrainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
