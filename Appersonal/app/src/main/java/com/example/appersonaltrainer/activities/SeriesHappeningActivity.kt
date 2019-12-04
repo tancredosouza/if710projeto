f51ac5b7b8dbe1839dde23cec60044d27f3757f7
Tantan <tjssj@cin.ufpe.br>
2019-12-04 12:50:35 -0300

    feature(SeriesHappeningaActivity): send notification after exercise ends

--- Appersonal/app/src/main/java/com/example/appersonaltrainer/activities/SeriesHappeningActivity.kt
+++ Appersonal/app/src/main/java/com/example/appersonaltrainer/activities/SeriesHappeningActivity.kt
@@ -1,27 +1,36 @@
 package com.example.appersonaltrainer.activities
 
-import android.content.BroadcastReceiver
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.LifecycleOwner
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.appersonaltrainer.R
-import com.example.appersonaltrainer.components.ExerciseTimer.Companion.CURRENT_EXERCISE_ENDED
 import com.example.appersonaltrainer.components.Time
 import com.example.appersonaltrainer.components.TimerState
 import com.example.appersonaltrainer.contract.AppersonalContract
 import com.example.appersonaltrainer.databases.SeriesDB
 import com.example.appersonaltrainer.model.Series
 import com.example.appersonaltrainer.view_model.AppersonalViewModel
-import kotlinx.android.synthetic.main.series_happening_activity.*
+import kotlinx.android.synthetic.main.series_happening_activity.current_exercise_remaining_time
+import kotlinx.android.synthetic.main.series_happening_activity.current_exercise_type
+import kotlinx.android.synthetic.main.series_happening_activity.next_exercise_button
+import kotlinx.android.synthetic.main.series_happening_activity.next_exercise_layout
+import kotlinx.android.synthetic.main.series_happening_activity.next_exercise_remaining_time
+import kotlinx.android.synthetic.main.series_happening_activity.next_exercise_type
+import kotlinx.android.synthetic.main.series_happening_activity.play_pause_button
+import kotlinx.android.synthetic.main.series_happening_activity.series_name
+import kotlinx.android.synthetic.main.series_happening_activity.stop_series_button
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
+
 
 class SeriesHappeningActivity : AppCompatActivity(), AppersonalContract.View {
     private lateinit var viewModel: AppersonalViewModel
@@ -104,7 +113,7 @@ class SeriesHappeningActivity : AppCompatActivity(), AppersonalContract.View {
         super.onDestroy()
     }
 
-    override fun updateImageResource(timerState : TimerState) {
+    override fun updateImageResource(timerState: TimerState) {
         if (timerState == TimerState.COUNTING) {
             play_pause_button.setImageResource(android.R.drawable.ic_media_pause)
         } else {
@@ -117,16 +126,48 @@ class SeriesHappeningActivity : AppCompatActivity(), AppersonalContract.View {
             getTimeFromSeconds(currentTimeInSeconds).toString()
 
         if (currentTimeInSeconds == 0L) {
+            sendNotification()
             goToNextExercise()
         }
     }
 
     private fun goToNextExercise() {
         if (e + 1 < seriesHappening.exercises.size) {
-            // TODO: send notification that exercise ended
             e += 1
             setupViewModelAndActivity(e)
             viewModel.handleButtonPress()
+        }
+    }
+
+    private fun sendNotification() {
+        createNotificationChannel()
+
+        val builder = NotificationCompat.Builder(this, "CHANNELID")
+            .setSmallIcon(R.drawable.notification_icon)
+            .setContentTitle("Appersonal")
+            .setContentText("Próximo exercício!")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(8978969, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "CHANNELNAME"
+            val descriptionText = "Appersonal Notification Channel"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("CHANNELID", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 
